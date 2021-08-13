a8876555794b3dfbc32ab25637a2d84494365366
Jose Perez <josemi.perezsolano@gmail.com>
2021-08-13 00:42:04 -0500

    cree un componente form y enlaze eventos para guardar informacion

--- src/pages/pagenew.js
+++ src/pages/pagenew.js
@@ -2,22 +2,31 @@ import React from 'react'
 import './styles/pagenew.css'
 import Navbar from '../components/navbar' 
 import Badge from '../components/badge' 
+import  Hero from '../assets/hero.jpg'
+import Form from '../components/form'
 
  function pagenew() {
     return (
         <div>
             <Navbar/>
             <div className="BadgeNew__hero">
-          <img className="img-fluid" src="{heade" alt="Logo" />
+          <img className=" img-hero" src={Hero} alt="Logo" />
         </div>
 
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-md-6">
               <Badge
                 
               />
             </div>
+
+            <div className="col-md-6">
+
+              <Form/>
+
+            </div>
+
           </div>
         </div>
       </div>
