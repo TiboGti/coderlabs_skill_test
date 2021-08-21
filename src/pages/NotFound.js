69216920d7efa642790e98cf8550430662dabe1f
Jose Perez <josemi.perezsolano@gmail.com>
2021-08-21 02:16:44 -0500

    nuevos archivos

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Notfound from '../assets/404.jpg'
+
+
+const NotFound = () => {
+    return (
+        <div className="Notfound">
+            <img src={Notfound} alt="" className="img-fluid"/>
+        </div>
+    )
+}
+
+export default NotFound
