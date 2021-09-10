e2fc6c1d4a8eb6523339cad05e05aaafcd5d87d1
Jose Perez <josemi.perezsolano@gmail.com>
2021-09-10 00:40:45 -0500

    separacion de componentes redundantes y un poco de purga

--- src/pages/BadgeDtails.js
+++ src/pages/BadgeDtails.js
@@ -1,46 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import './styles/BadgeDetails.css';
 import confLogo from '../assets/hero.jpg';
-import PageLoading from '..//components/loader';
-import PageError from '../components/pageError';
 import Badge from '../components/Badge';
-import api from '../services/api';
 
-class BadgeDetails extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    data: undefined,
-  };
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-fetchData = async () => {
-    this.setState({ loading: true, error: null });
-
-    try {
-        const data = await api.badges.read(this.props.match.params.badgeId);
-        this.setState({ loading: false, data: data });
-    } catch (error) {
-        this.setState({ loading: false, error: error });
-    }
-};
-
-render() {
-    if (this.state.loading) {
-        return <PageLoading />;
-    }
-
-    if (this.state.error) {
-        return <PageError error={this.state.error} />;
-    }
-
-    const badge = this.state.data;
-
+function BadgeDetails(props) {
+  const badge = props.badge;
 return (
     <div>
     <div className="BadgeDetails__hero">
@@ -90,7 +55,6 @@ return (
         </div>
       </div>
     );
-  }
 }
 
 export default BadgeDetails;
